f2ac21893c97fc67c765e99d2a07517cb1260d9f
Alumno <ulsa@iMacD105.local>
2019-05-28 13:12:16 -0700

    actualizacion

--- Laravel/Medicamentos/app/Http/Controllers/DashboardController.php
+++ Laravel/Medicamentos/app/Http/Controllers/DashboardController.php
@@ -4,10 +4,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     public function dashboard() {
         return view('dashboard.dashboard');
     }
