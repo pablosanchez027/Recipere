6583309ef20684beabd0603cbe8fa3e3d39c1c00
pablosanchez027 <ulsa@iMacD107.local>
2019-05-28 10:30:44 -0700

    Table View

---
+++ Xcode/Recipere/celdaMedicina.swift
@@ -0,0 +1,19 @@
+//
+//  celdaMedicina.swift
+//  Recipere
+//
+//  Created by Alumno on 5/28/19.
+//  Copyright © 2019 Pablo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class celdaMedicina : UITableViewCell {
+    @IBOutlet weak var lblMedicinaNombre: UILabel!
+    @IBOutlet weak var lblMedicinaDosis: UILabel!
+    @IBOutlet weak var lblMedicinaHora: UILabel!
+    @IBOutlet weak var lblMedicinaDia: UILabel!
+    @IBOutlet weak var imgMedicinaLogo: UIImageView!
+    
+}
